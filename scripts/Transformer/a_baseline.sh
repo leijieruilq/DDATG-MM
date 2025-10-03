5eb0492012bd407c56ae595e0ca20394aa917969
leijierui <95903944+leijieruilq@users.noreply.github.com>
2025-10-03 16:18:30 +0800

    Add files via upload

---
+++ scripts/Transformer/a_baseline.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# 进入脚本所在目录
+cd /data/ljr/keyan/MM-TSFlib-main/scripts/Transformer
+
+# 运行所有 .sh 文件，但排除 _gnn.sh 结尾的
+for script in *.sh; do
+    if [[ $script != *_gnn.sh ]]; then
+        echo "正在运行: $script"
+        bash "$script"
+    fi
+done
---
+++ scripts/Transformer/a_baseline_gnn.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# 进入脚本所在目录
+cd /data/ljr/keyan/MM-TSFlib-main/scripts/Transformer
+
+# 运行所有 .sh 文件
+for script in *_gnn.sh; do
+    echo "正在运行: $script"
+    bash "$script"
+done
